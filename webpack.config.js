a7da24e98070ee730f06dcc42ffc9b749db07966
Anastasiya Soboleva <soboleva@tiburon-research.ru>
2023-01-20 17:14:46 +0300

    настраиваем babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        main: './src/index.js'
+        main: ['@babel/polyfill', './src/index.js']
     },
     output: {
         filename: '[name].bundle.js',
@@ -49,6 +49,17 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            }
         ]
     },
 
