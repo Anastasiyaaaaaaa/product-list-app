a5991e3b34f331d8ae40a1ec0f9c7e1920faeb26
Anastasiya Soboleva <soboleva@tiburon-research.ru>
2023-01-21 22:27:36 +0300

    продолжаем верстать

--- src/components/Cards/Card/index.jsx
+++ src/components/Cards/Card/index.jsx
@@ -6,27 +6,30 @@ export const Card = ({ data }) => {
     return <div className='card'>
         <div className="card__border">
             <div className="card__content">
-                <div className='card__header'>Сказочное заморское яство</div>
-                <div className='card__title'> {data.name} </div>
-                <div className='card__subtitle'> {data.taste} </div>
 
-                <div className='card__text'>
-                    <div className='card__comment'> {data.quantity} </div>
-                    <div className='card__comment'> {data.present} </div>
-                    <div className='card__comment'> {data.dop_info} </div>
+                <div className="card__content-container">
+                    <div className='card__header'>Сказочное заморское яство</div>
+                    <div className='card__title'> {data.name} </div>
+                    <div className='card__subtitle'> {data.taste} </div>
+
+                    <div className='card__text'>
+                        <div className='card__comment'> {data.quantity} </div>
+                        <div className='card__comment'> {data.present} </div>
+                        <div className='card__comment'> {data.dop_info} </div>
+                    </div>
                 </div>
 
                 <div className='card__foto'>
                     <img alt='cat foto' src={catFoto} />
                 </div>
 
-                <div className='circle_blue'>
+                <div className='circle'>
                     {data.weight} кг
                 </div>
             </div>
         </div>
         <div className='card__footer'>
-            <p>Чего сидишь? Порадуй котэ, 
+            <p>Чего сидишь? Порадуй котэ,
                 <button className='card__button' type='button'>купи.</button></p>
         </div>
     </div>
