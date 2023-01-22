25da8c6ef0efe1f78e041e426fbf0836a685eb72
Anastasiya Soboleva <soboleva@tiburon-research.ru>
2023-01-22 14:33:29 +0300

    выносим кружок в отдельную компоненту + настраиваем поведение карточки

--- src/components/Cards/Card/index.jsx
+++ src/components/Cards/Card/index.jsx
@@ -1,28 +1,39 @@
 import React, { useState } from 'react'
 import './style.css'
 import catFoto from './img/cat-photo.png'
+import { Circle } from '../Circle';
 
 export const Card = ({ data }) => {
 
-    const [isSelected, setIsSelected] = useState(false);
-    const [isHovered, setIsHovered] = useState(false);
+    const textDefault = 'Сказочное заморское яство'
+    const textSelHover = 'Котэ не одобряет?'
+
+
+    const [isSelected, setIsSelected] = useState(false)
+    const [cardHeader, setCardHeader] = useState(textDefault)
 
     const handleClick = () => {
         setIsSelected(!isSelected)
     }
+
     const handleMouseOver = () => {
-        setIsHovered(true)
+        if (isSelected) {
+            setCardHeader(textSelHover)
+        }
     }
+
     const handleMouseOut = () => {
-        setIsHovered(false)
+        setCardHeader(textDefault)
     }
 
     return <div className={`card_${isSelected ? 'selected' : 'default'}`}>
         <div className="card__border" onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
             <div className="card__content">
 
-                <div className="card__text-container"> 
-                        <div className='card__header'>Сказочное заморское яство</div> 
+                <div className="card__text-container">
+                    <div className='card__header'>
+                        {cardHeader}
+                    </div>
                     <div className='card__title'> {data.name} </div>
                     <div className='card__subtitle'> {data.taste} </div>
 
@@ -33,12 +44,13 @@ export const Card = ({ data }) => {
                     </div>
                 </div>
 
+
                 <div className='card__foto'>
                     <img alt='cat foto' src={catFoto} />
                 </div>
 
-                <div className='circle'>
-                    {data.weight} кг
+                <div className="card__elem_right-bottom">
+                    <Circle text={`${data.weight} кг`}/> 
                 </div>
             </div>
         </div>
---
+++ src/components/Cards/Circle/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './style.css'
+
+export const Circle = ({ text }) => {
+    return <div className='circle'>
+        {text}
+    </div>
+}
